cfca8e60800c48bfe5996b48b2d231522672e6ed
Zeph180 <zephrichards1@gmail.com>
2023-04-18 12:28:13 +0300

    Add class student

---
+++ student.rb
@@ -0,0 +1,12 @@
+class Student < Person
+  def initialize(id, age, classroom, name = 'Unknown', parent_permission = true)
+    super(id, name, age, parent_permission)
+    @classroom = classroom
+  end
+
+  attr_accessor :classroom
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
+end
