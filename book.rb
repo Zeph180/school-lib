98a0e0aabd16b0af605621ce68cfdd04ce8c8754
Zeph180 <zephrichards1@gmail.com>
2023-04-19 12:53:42 +0300

    Add class book

---
+++ book.rb
@@ -0,0 +1,17 @@
+class Book
+  attr_accessor :title, :author, :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals << rental
+  end
+
+  def remove_rental(rental)
+    @rentals.delete(rental)
+  end
+end
